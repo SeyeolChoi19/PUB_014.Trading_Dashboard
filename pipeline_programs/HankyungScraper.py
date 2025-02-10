164b0c7ae376e1db459e5d9a0d5c789ae042018f
SeyeolChoi19 <78524895+SeyeolChoi19@users.noreply.github.com>
2025-02-10 15:34:39 +0900

    Update HankyungScraper.py

--- pipeline_programs/HankyungScraper.py
+++ pipeline_programs/HankyungScraper.py
@@ -16,7 +16,7 @@ class HankyungScraper:
         self.hankyung_news_table_name   = hankyung_news_table_name 
         self.target_date                = target_date
         self.server_schema_name         = server_schema_name 
-        self.article_dictionary_columns = ["article_date", "article_source", "article_title", "article_text"]
+        self.article_dictionary_columns = ["article_date", "article_source", "article_link", "article_title", "article_text"]
         self.news_articles_dictionary   = []
         self.__database_interface       = DBInterface()
         self.__database_interface.connection_settings(sql_type, os.getenv("ADMIN_NAME"), os.getenv("ADMIN_PWD"), hostname, server_name)
@@ -26,8 +26,8 @@ class HankyungScraper:
             for (article_date, article_url, article_title) in zip(article_dates, article_urls, titles_list):
                 sub_date = str(article_date)[0:10]
 
-                if ((sub_date >= self.target_date) and (self.__database_interface.check_if_data_exists_in_column(self.hankyung_news_table_name, "article_source", article_url, self.server_schema_name))):
-                    results_list = [sub_date, article_url, article_title]
+                if ((sub_date >= self.target_date) and (self.__database_interface.check_if_data_exists_in_column(self.hankyung_news_table_name, "article_link", article_url, self.server_schema_name)[0][0] != 1)):
+                    results_list = [sub_date.replace(".", "-"), "hankyung", article_url, article_title]
                     self.news_articles_dictionary.append(results_list)
             
             return min(article_dates)
@@ -36,8 +36,8 @@ class HankyungScraper:
 
         while (article_date >= self.target_date):
             self.selenium_object.driver.get(self.hankyung_news_portal.format(page_counter))
-            elements_list = self.selenium_object.find_nested_elements("news-tit", self.selenium_object.wait_for_element_and_return_element("news-list"), "css")
-            article_dates = [element.text.replace(".", "-") for element in self.selenium_object.find_nested_elements("txt-date", self.selenium_object.search_for_element("news-list"))]
+            elements_list = self.selenium_object.find_nested_elements("div.txt-cont h3.news-tit a", hankyung_scraper.selenium_object.search_for_element("news-list"), "css")
+            article_dates = [element.text for element in self.selenium_object.find_nested_elements("txt-date", self.selenium_object.search_for_element("news-list"))]
             article_urls  = [element.get_attribute("href") for element in elements_list]
             titles_list   = [element.text for element in elements_list]
             article_date  = save_article_data()
@@ -45,46 +45,20 @@ class HankyungScraper:
 
     def collect_article_texts(self):
         for sub_list in self.news_articles_dictionary:
-            self.selenium_object.driver.get(sub_list[1])
+            self.selenium_object.driver.get(sub_list[2])
             article_text = self.selenium_object.wait_for_element_and_return_element("article-body").text if (self.selenium_object.check_for_element("article-body")) else ""
             sub_list.append(article_text)
 
+    def upload_data_to_database(self):
+        output_dataframe = pd.DataFrame(self.news_articles_dictionary, columns = self.article_dictionary_columns)
+        self.__database_interface.upload_to_database(self.hankyung_news_table_name, output_dataframe, schema_name = self.server_schema_name)
 
+if (__name__ == "__main__"):
+    with open("./config/BackendConfig.json", "r", encoding = "utf-8") as f:
+        config_dict = json.load(f)
 
-# selenium_object = SeleniumSettings("./config/chromedriver.exe", 30)
-# selenium_object.driver_settings()
-# selenium_object.driver.get("https://www.hankyung.com/financial-market")
-# len([element.text for element in selenium_object.search_for_elements("news-tit")])
-
-# selenium_object.search_for_element("/html/body/div/div[2]/div/div[1]/div[3]/div/div[2]/ul/li[1]/div[1]/h3/a", "xpath").text
-
-
-
-# import selenium
-
-# isinstance(selenium_object.search_for_element("/html/body/div/div[2]/div/div[1]/div/div/div[2]/ul/li[1]/div[1]/h3/a", "xpath"), selenium.webdriver.remote.webelement.WebElement)
-
-
-# from selenium.webdriver.common.by import By
-
-# [element.text for element in selenium_object.search_for_element("news-list").find_elements(By.CLASS_NAME, "news-tit")]
-
-
-
-
-# selenium_object.search_for_element("/html/body/div/div[2]/div/div[1]/div[3]/div/div[2]/ul/li[1]/div[1]/h3/a", "xpath").get_attribute("href")
-
-# len([element.text for element in selenium_object.driver.find_element(By.CLASS_NAME, "news-list").find_elements(By.CSS_SELECTOR, "div.txt-cont h3.news-tit a")])
-
-# [element for element in selenium_object.find_nested_elements("div.txt-cont h3.news-tit a", selenium_object.search_for_element("news-list"), "css")]
-
-# selenium_object.search_for_element("article-body").text
-
-
-# new_list = [[1,2,3,4], [1,2,3,4], [1,2,3,4], [1,2,3,4]]
-
-# for sub_list in new_list:
-#     sub_list.append(5)
-
-
-# new_list
+    hankyung_scraper = HankyungScraper(SeleniumSettings(**config_dict["HankyungScraper"]["constructor"]))
+    hankyung_scraper.hankyung_scraper_settings_method(**config_dict["HankyungScraper"]["hankyung_scraper_settings_method"])
+    hankyung_scraper.collect_hankyung_news_urls()
+    hankyung_scraper.collect_article_texts()
+    hankyung_scraper.upload_data_to_database()
