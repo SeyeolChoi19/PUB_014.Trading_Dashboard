bd8da1054f63df453d151770c29f18e7bba75082
SeyeolChoi19 <78524895+SeyeolChoi19@users.noreply.github.com>
2025-02-14 10:37:14 +0900

    Update custom_exceptions.py

--- custom_exceptions/custom_exceptions.py
+++ custom_exceptions/custom_exceptions.py
@@ -1,10 +1,10 @@
 from typing import Callable
 
 def extraction_exception(inner_function: Callable):
-    def inner_function(*args, **kwargs):
+    def wrapper_function(*args, **kwargs):
         try:
             inner_function(*args, **kwargs)
         except Exception as E:
             pass 
     
-    return inner_function
+    return wrapper_function
