0328b497ba742f95b866f7711e3a908443aeb45c
SeyeolChoi19 <78524895+SeyeolChoi19@users.noreply.github.com>
2025-02-14 12:01:59 +0900

    Update custom_exceptions.py

--- custom_exceptions/custom_exceptions.py
+++ custom_exceptions/custom_exceptions.py
@@ -8,3 +8,12 @@ def extraction_exception(inner_function: Callable):
             pass 
     
     return wrapper_function
+
+def database_upload_exception(inner_function: Callable):
+    def wrapper_function(*args, **kwargs):
+        try: 
+            inner_function(*args, **kwargs)
+        except Exception as E: 
+            pass 
+
+    return wrapper_function
