383f0205fe20a2f04d824292f7e27dd15b6a63b2
SeyeolChoi19 <78524895+SeyeolChoi19@users.noreply.github.com>
2025-02-14 11:21:59 +0900

    Update MojitoInterface.py

--- pipeline_programs/MojitoInterface.py
+++ pipeline_programs/MojitoInterface.py
@@ -10,11 +10,11 @@ from concurrent.futures                  import ThreadPoolExecutor
 from custom_exceptions.custom_exceptions import extraction_exception
 
 class MojitoInterface:
-    def mojito_interface_settings_method(self, sql_type: str, hostname: str, server_name: str, schema_name: str):
-        self.schema_name = schema_name 
-        self.output_data = []
-        self.thread_lock = Lock()
-        self.__db_object = DBInterface()
+    def mojito_interface_settings_method(self, sql_type: str, hostname: str, server_name: str, schema_name: str, mojito_table: str):
+        self.schema_name  = schema_name 
+        self.mojito_table = mojito_table
+        self.thread_lock  = Lock()
+        self.__db_object  = DBInterface()
         self.__db_object.connection_settings(sql_type, os.getenv("ADMIN_NAME"), os.getenv("ADMIN_PWD"), hostname, server_name)
 
         self.__api_interface = mojito.KoreaInvestment(
@@ -32,11 +32,12 @@ class MojitoInterface:
                 with self.thread_lock:
                     self.output_data.append(company_dataframe)
             
-        start_date, end_date = [date_string.strip() for date_string in date_range.split("~")]
+        self.output_data, start_date, end_date = [], [date_string.strip() for date_string in date_range.split("~")]
 
         with ThreadPoolExecutor(max_workers = 4) as executor:
             for symbol in stocks_list: 
                 executor.submit(extract_data, start_date, end_date, symbol)
-
-            executor.shutdown(True)
-            
+    
+    def upload_data_to_database(self):
+        output_dataframe = pd.concat(self.output_data).reset_index(drop = True)
+        self.__db_object.upload_to_database(self.mojito_table, output_dataframe, schema_name = self.schema_name)
